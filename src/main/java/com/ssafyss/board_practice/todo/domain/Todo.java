c730167a344019e29bf9404b8fde299d89013a51
janghyoseon <hosu0125@naver.com>
2024-10-24 21:29:06 +0900

    feat: Todo 엔티티 생성 예시

---
+++ src/main/java/com/ssafyss/board_practice/todo/domain/Todo.java
@@ -0,0 +1,11 @@
+package com.ssafyss.board_practice.todo.domain;
+
+/**
+ * 이것은 예시일 뿐 그대로 사용하지 말 것!
+ */
+public class Todo {
+    private Long id;
+    private String comment;
+    private boolean checked;
+    private boolean deleted;
+}
